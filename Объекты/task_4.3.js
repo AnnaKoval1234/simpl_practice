0457003dc57ad3effbbed112c295d41b9f646d43
AnnaKoval1234 <anna_koval_02@bk.ru>
2023-10-27 17:28:32 +0500

    Закончила раздел "Объекты: основы"

---
+++ Объекты/task_4.3.js
@@ -0,0 +1,19 @@
+// У нас есть объект ladder (лестница), который позволяет подниматься и спускаться:
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() { // показывает текущую ступеньку
+    alert( this.step );
+    return this;
+  }
+};
+
+//Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
